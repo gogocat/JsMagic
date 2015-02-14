a3dc7892c43f9759326895dedf8b8c1037a26a06
gogocat <adamchow2326@yahoo.com.au>
2015-02-14 20:42:18 +1100

    updated test module

--- require/modules/hello.js
+++ require/modules/hello.js
@@ -2,6 +2,9 @@
 var testVar = "shouldn't see this!";
 
 exports.init = function(arg) {
-	//debugger;
-	alert(arg);
+	var bodyNode = document.getElementsByTagName("body")[0],
+		contentNode = document.createElement("p"),
+		textNode = document.createTextNode(arg); 
+	
+	bodyNode.appendChild(contentNode.appendChild(textNode));
 };
