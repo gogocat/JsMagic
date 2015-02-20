7a4ed3596643f983a02650f2183ae7934aa01afd
gogocat <adamchow2326@yahoo.com.au>
2015-02-20 23:28:48 +1100

    added work in progress new version

---
+++ require/js/jqueryRequire2.js
@@ -0,0 +1,91 @@
+/*
+//	require script loader
+//	require jQuery
+//	if environment is Node js it will do nothing.
+//	@uri - script location
+//	@callback - callback function for Asynchronous load script
+*/
+(function($, env) {
+	"use strict";
+	var isNode = (env !== window && typeof module !== "undefined" && module.exports),
+		supportAmd = (typeof env.define === "function" && env.define.amd),
+		onFail,
+		require,
+		cache = {};
+		
+	if (isNode) {
+		return;
+	}
+		
+	require = function (uri, callback) {
+		var isAsync = (typeof callback === "function") ? true : false,
+			dependenceList = [],
+			request,
+			wrapScript,
+			ret;
+		
+		if (typeof uri === "string") {
+			dependenceList.push(uri);
+		} else if ($.isArray(uri)) {
+			dependenceList = uri;
+		}
+		
+		if (cache[uri]) {
+			if (isAsync) {
+				return callback(cache[uri]);
+			}
+			return cache[uri];
+		}
+		
+		wrapScript = function(responseText, dataType) {
+			var closureFn,
+				closureFnText,
+				source;
+				
+			//TODO: consider support AMD.
+			// static analysis to wrap script as AMD module.
+			if (responseText) {	
+				closureFnText = '"use strict";\n var module = {}, exports = {}; \n';
+				closureFnText += responseText;
+				closureFnText += '\n if(!module.exports) module.exports = exports; \n module.id = "'+ uri +'";';
+				closureFnText += '\n return module.exports; \n //# sourceURL='+ uri;
+				closureFn = new Function(closureFnText);
+				cache[uri] = source = closureFn(); // Make the closureFn
+				return source;
+			} 
+			return null;
+		};
+
+		request = $.ajax({
+			url: uri,
+			type: 'GET',
+			dataType: "script",
+			async: isAsync,
+			cache:true,
+			crossDomain: false,
+			dataFilter: wrapScript,
+			success: function(closureFn) {
+				if(!closureFn) {
+					return;
+				}
+				if (isAsync) {
+					callback(closureFn);
+				} else {
+					ret = closureFn;
+				}
+			},
+			error: onFail
+		});
+		
+		$.when(request, request).done(callback).fail();
+		
+		return (isAsync) ? request : ret;
+	};
+	
+	onFail = function() {
+		console.log("fail: ", arguments);
+	}
+	
+	env.require = require;
+
+}(jQuery, this));
