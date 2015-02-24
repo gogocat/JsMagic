8c09ec53a37be5b7b8b96f2f6b4f473813593ea6
gogocat <adamchow2326@yahoo.com.au>
2015-02-24 21:53:14 +1100

    WIP

--- require/js/jqueryRequire2.js
+++ require/js/jqueryRequire2.js
@@ -99,7 +99,7 @@
 					request(value);
 				);
 			});
-			// TODO: move this to another function
+
 			$.when.apply($, deferreds).done(callback).fail(onFail);
 		}
 		return (isAsync) ? request : ret;
